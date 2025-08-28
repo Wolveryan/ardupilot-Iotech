9b347678e38a02d1becd7f1c05ab63cbaa4ef10a
Wolveryan <152795206+Wolveryan@users.noreply.github.com>
2025-08-28 14:44:37 +0530

    Update AP_Proximity_PYSITL.h

--- libraries/AP_Proximity/AP_Proximity_PYSITL.h
+++ libraries/AP_Proximity/AP_Proximity_PYSITL.h
@@ -5,6 +5,5 @@ class AP_Proximity_PYSITL : public AP_Proximity_Backend {
 public:
     AP_Proximity_PYSITL(AP_Proximity &frontend) : AP_Proximity_Backend(frontend) {}
 
-    // Adjust based on what the backend API in your old repo actually requires
-    void update() {}  
+    void update() override {}  // mark it override to silence -Werror
 };
