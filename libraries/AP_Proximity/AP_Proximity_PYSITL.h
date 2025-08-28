9384a7433d58687a9beaa4b3cf54293872b29c6d
Wolveryan <152795206+Wolveryan@users.noreply.github.com>
2025-08-28 15:44:53 +0530

    Update AP_Proximity_PYSITL.h

--- libraries/AP_Proximity/AP_Proximity_PYSITL.h
+++ libraries/AP_Proximity/AP_Proximity_PYSITL.h
@@ -3,6 +3,12 @@
 
 class AP_Proximity_PYSITL : public AP_Proximity_Backend {
 public:
-    AP_Proximity_PYSITL(AP_Proximity &_frontend) : AP_Proximity_Backend(_frontend) {}
+    AP_Proximity_PYSITL(AP_Proximity &_frontend, AP_Proximity::Proximity_State &_state) 
+        : AP_Proximity_Backend(_frontend, _state) {}
+    
     void update() override {}  // mark it override to silence -Werror
+    
+    // Required pure virtual methods from base class
+    float distance_max() const override { return 100.0f; }  // or appropriate max distance
+    float distance_min() const override { return 0.1f; }    // or appropriate min distance
 };
